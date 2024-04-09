ab903c9f541f03515ac0b2f868814711e8e3b924
Min RK <benjaminrk@gmail.com>
2024-04-09 17:31:48 +0200

    support read-only acces in jupyter-fs

--- jupyterhub/images/user/jupyter_server_config.py
+++ jupyterhub/images/user/jupyter_server_config.py
@@ -1,10 +1,12 @@
 import os
+import sys
 from configparser import ConfigParser
 from pathlib import Path
 from urllib.parse import urlparse
 
 import fs.errors
 import fs.opener
+from fs.info import Info, ResourceType
 from fs_s3fs import S3FS
 from fs_s3fs.opener import S3FSOpener
 
@@ -33,27 +35,36 @@ c.ContentsManager.hide_globs.extend(["lost+found"])
 # workaround https://github.com/PyFilesystem/s3fs/issues/70
 # because our files weren't created with S3FS (aka fs-s3fs)
 # they were created with s3fs. Ha!
+
+
 class EnsureDirS3FS(S3FS):
-    def getinfo(self, path, *args, **kwargs):
+    def getinfo(self, path, namespaces=None):
         try:
-            return super().getinfo(path, *args, **kwargs)
+            return super().getinfo(path, namespaces)
         except fs.errors.ResourceNotFound as e:
-            # check if getinfo failed becuase it's a directory
-            # without an empty Object
+            # workaround https://github.com/PyFilesystem/s3fs/issues/70
+            # check if it's a directory
             # if so, create it
-            # S3FS.scandir and S3FS.getinfo don't work on missing directories
-            # but S3FS.listdir does
+            # scandir/getinfo don't work on missing directories
+            # but listdir does
+            # if it's really a directory, return stub Info
             try:
                 self.listdir(path)
             except fs.errors.ResourceNotFound:
-                # it actually doesn't exist, raise original error
                 raise e from None
             else:
-                # it's a directory but the empty directory object doesn't exist
-                # create it then call getinfo
-                print(f"Making empty directory {path}")
-                self.makedir(path)
-                return super().getinfo(path, *args, **kwargs)
+                # return fake Info
+                # based on getinfo handling of root (`/`)
+                name = path.rstrip("/").rsplit("/", 1)[-1]
+                return Info(
+                    {
+                        "basic": {
+                            "name": name,
+                            "is_dir": True,
+                        },
+                        "details": {"type": int(ResourceType.directory)},
+                    }
+                )
 
 
 # define custom opener for GFTS
@@ -72,7 +83,6 @@ class GFTSOpener(S3FSOpener):
             "aws_endpoint_url",
             fallback="https://s3.gra.perf.cloud.ovh.net",
         )
-        # get 'gra' from 's3.gra.perf....'
         region_name = urlparse(endpoint_url).hostname.split(".")[1]
         return EnsureDirS3FS(
             bucket_name,
@@ -91,11 +101,30 @@ class GFTSOpener(S3FSOpener):
 fs.opener.registry.install(GFTSOpener)
 
 
-c.JupyterFs.resources = [
-    {
-        "name": "gfts-data-lake",
-        "url": "gfts-s3://destine-gfts-data-lake/",
-    },
-]
+c.JupyterFs.resources = fs_resources = []
+
+bucket_env = os.environ.get("JUPYTER_FS_BUCKETS")
+
+if bucket_env:
+    buckets = bucket_env.split(",")
+else:
+    buckets = []
+
+for bucket in buckets:
+    url = f"gfts-s3://{bucket}/"
+    try:
+        with fs.open_fs(url) as s3_fs:
+            list(s3_fs.scandir("/"))
+    except Exception as e:
+        print(f"Error listing {url}: {e}", file=sys.stderr)
+        continue
+    else:
+        print(f"Mounting {url}", file=sys.stderr)
+        fs_resources.append(
+            {
+                "name": bucket,
+                "url": url,
+            }
+        )
 
 c.ServerApp.contents_manager_class = "jupyterfs.metamanager.MetaManager"
