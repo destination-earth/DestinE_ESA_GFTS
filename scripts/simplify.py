2d5e21cae84f8bb7773c06e89e9600ed58055848
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2024-09-20 15:42:47 +0100

    Use remote files for simplify

--- scripts/simplify.py
+++ scripts/simplify.py
@@ -3,90 +3,118 @@ import logging
 import pandas as pd
 import geopandas as gpd
 import xarray as xr
+import boto3
+import s3fs
+from multiprocessing import Pool
 
 logging.basicConfig()
 
 logger = logging.getLogger("gfts")
 logger.setLevel(logging.DEBUG)
-
+ENDPOINT = "https://s3.gra.perf.cloud.ovh.net/"
+BUCKET = "gfts-ifremer"
+PREFIX = "tags/bargip/tracks_4/"
+PROFILE = "ovh_gfts"
+REGION = "gra"
 NR_OF_CELLS_PER_TIMESLICE = 200
+POOL = 10
+
+
+boto3.setup_default_session(profile_name=PROFILE)
 
 
 def simplify(tag):
     logger.debug(f"Opening tag {tag}")
-    data = xr.open_zarr(f"data/{tag}.zarr")
+
+    fsg = s3fs.S3FileSystem(
+        anon=False,
+        profile=PROFILE,
+        client_kwargs={
+            "endpoint_url": ENDPOINT,
+            "region_name": REGION,
+        },
+    )
+    store = s3fs.S3Map(
+        root=f"s3://{BUCKET}/{PREFIX}{tag}/states.zarr",
+        s3=fsg,
+        check=False,
+    )
+    data = xr.open_zarr(store=store)
 
     vars_to_keep = ["states", "latitude", "longitude", "time"]
     vars_to_drop = [var for var in data.variables if var not in vars_to_keep]
-    logger.debug(f"Dropping vars {vars_to_drop}")
     data = data.drop_vars(vars_to_drop)
 
-    # max_probability = data.states.max().values
-    # threshold = max_probability * 1e-4
-
-    # logger.debug(f"Threholding data to {threshold}")
-    # data = data.where(data > threshold)
-
-    # Do one by one to save memory. Most timestamps have
-    # a lot of nodata.
-    logger.debug("Creating dataframe")
+    # Do one by one to save memory. Most timestamps have a lot of nodata
+    # that can be dropped in the loop. If done in one step this will grow fast.
     dfs = []
-    # for time, grp in data.groupby("time"):
     for time in data.time:
-        logger.debug(f"Working on time {time.values}")
+        if len(dfs) % 25 == 0:
+            logger.debug(f"Working on time {tag} {time.values}")
         grp = data.where(data.time == time, drop=True)
-        grp = grp.where(data.states > 1e-6)
         df = grp.to_dataframe().dropna().reset_index()
         del df["x"]
         del df["y"]
         df = df.sort_values("states", ascending=False)
         dfs.append(df[:NR_OF_CELLS_PER_TIMESLICE])
-        if len(dfs) > 10:
-            break
 
-    combined = pd.concat(dfs)
-
-    combined = gpd.GeoDataFrame(
-        combined[["time", "states"]],
-        geometry=gpd.points_from_xy(combined.longitude, combined.latitude),
-        crs="EPSG:4326",
-    )
-
-    # Convert probability to integer
-    max_value = combined["states"].max()
-    combined["states"] = (combined["states"] / max_value * 65535).astype("uint16")
-
-    return combined
+    return pd.concat(dfs)
 
 
 def to_geojson(df, tag):
-    highest_probability = (
-        df.sort_values("states", ascending=False)
-        .drop_duplicates("time")
-        .sort_values("time")
+    logger.debug(f"Writing geojson file for {tag}")
+
+    track = df.sort_values("states", ascending=False).drop_duplicates("time")
+    track = track.sort_values("time")
+
+    geom = gpd.points_from_xy(track.longitude, track.latitude)
+    track = gpd.GeoDataFrame(
+        track[["time", "states"]],
+        geometry=geom,
+        crs="EPSG:4326",
     )
-    highest_probability.to_file("data/bla.gejson", driver="GeoJSON")
+    track.to_file(f"data/{tag}.geojson", driver="GeoJSON")
 
 
 def to_parquet(df, tag):
+    logger.debug(f"Writing parquet file for {tag}")
     pq = df.copy()
-    pq["longitude"] = (pq["longitude"] * 1e6).astype("int16")
-    pq["latitude"] = (pq["latitude"] * 1e6).astype("int16")
+
+    pq["longitude"] = (pq["longitude"] * 1e6).astype("int32")
+    pq["latitude"] = (pq["latitude"] * 1e6).astype("int32")
+
+    # Convert probability to integer
+    max_value = pq["states"].max()
+    pq["states"] = (pq["states"] / max_value * 65535).astype("uint16")
 
     pq = pq.set_index(["time", "longitude", "latitude"])
+
     pq.to_parquet(f"data/{tag}.parquet")
 
+    df.to_parquet(f"data/{tag}_orig.parquet")
 
-def main(tag):
-    logger.debug("Simplyfing")
+
+def list_tags():
+    s3 = boto3.resource(service_name="s3", endpoint_url=ENDPOINT)
+
+    folders = s3.meta.client.list_objects(Bucket=BUCKET, Prefix=PREFIX, Delimiter="/")
+    tags = [dat["Prefix"].split("/")[-2] for dat in folders["CommonPrefixes"]]
+    return tags
+
+
+def process_tag(tag):
     df = simplify(tag=tag)
-    logger.debug("Writing geojson")
     to_geojson(df=df, tag=tag)
-    logger.debug("Writing parquet")
     to_parquet(df=df, tag=tag)
 
 
+def main():
+    logger.debug("Listing tags")
+    tags = list_tags()
+
+    with Pool(POOL) as pl:
+        pl.map(process_tag, tags)
+
+
 if __name__ == "__main__":
-    # aws s3 --profile ovh_gfts --endpoint-url https://s3.gra.perf.cloud.ovh.net/ ls s3://gfts-ifremer/tags/bargip/
-    main("AD_A11806")
-    # main("CB_A11071")
+    main()
