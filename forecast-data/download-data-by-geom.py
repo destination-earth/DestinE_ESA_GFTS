080fdd0e78ece9a88e8e515ec045e525ee45f510
Daniel Wiesmann <yellowcap@users.noreply.github.com>
2025-05-26 14:01:47 +0100

    Update download data script

--- forecast-data/download-data-by-geom.py
+++ forecast-data/download-data-by-geom.py
@@ -1,63 +1,8 @@
-import earthkit.data
-import xarray as xr
+"""
+Download ocean forecast data by geometry
 
-AOI = {
-    "type": "Feature",
-    "properties": {
-        "name": "Download Area",
-        "description": "Polygon area for climate forecast data download - North Atlantic region",
-    },
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-            [
-                [-14.65, 40.92],
-                [9.40, 40.92],
-                [9.40, 56.74],
-                [-14.65, 56.74],
-                [-14.65, 40.92],
-            ]
-        ],
-    },
-}
-
-AOI_SMALL = {
-    "type": "Feature",
-    "properties": {
-        "name": "Download Area",
-        "description": "Polygon area for climate forecast data download - North Atlantic region",
-    },
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-            [
-                [-4.302114705361248, 47.84753399497097],
-                [-4.302114705361248, 45.613524433333595],
-                [-1.9232431194369894, 45.613524433333595],
-                [-1.9232431194369894, 47.84753399497097],
-                [-4.302114705361248, 47.84753399497097],
-            ]
-        ],
-    },
-}
-
-
-def get_month_dates(year, month):
-    """Generate a string of dates for a given year and month in format YYYYMMDD.
-    Returns dates separated by '/' for all days in the month."""
-    import calendar
-
-    # Get number of days in the month
-    _, num_days = calendar.monthrange(year, month)
-
-    # Generate list of date strings
-    dates = []
-    for day in range(1, num_days + 1):
-        date_str = f"{year}{month:02d}{day:02d}"
-        dates.append(date_str)
-
-    return "/".join(dates)
-
+This script downloads ocean forecast data from DestinE poytope
+for a specified area of interest (AOI) defined by coordinates.
 
 # Data catalogue:
 
@@ -77,8 +22,38 @@ def get_month_dates(year, month):
 # 263100, Time-mean sea surface practical salinity, avg_sos, g kg**-1, ICON/IFS-NEMO/IFS-FESOM
 # 263101, Time-mean sea surface potential temperature, avg_tos, K, ICON/IFS-NEMO/IFS-FESOM
 
+# Time range
+https://destine.ecmwf.int/climate-change-adaptation-digital-twin-climate-dt/#1737392094446-739c56b7-c2b2
 
-def get_data(model, param, date):
+Future projection
+	ICON 	5km across Earth system components 	2020-2039
+    IFS-NEMO 	4.4 km atmosphere, 1/12 ocean/sea-ice 	2020-2039
+"""
+import calendar
+from concurrent.futures import ThreadPoolExecutor
+
+import earthkit.data
+
+
+def get_month_dates(year, month):
+    """Generate a string of dates for a given year and month in format YYYYMMDD.
+    Returns dates separated by '/' for all days in the month."""
+
+    _, num_days = calendar.monthrange(year, month)
+
+    dates = []
+    for day in range(1, num_days + 1):
+        date_str = f"{year}{month:02d}{day:02d}"
+        dates.append(date_str)
+
+    return "/".join(dates)
+
+
+def get_data(model, param, dates, shape):
+    print(
+        f"Downloading data for model {model}, param {param}, dates {dates[:8]} {dates[-8:]}"
+    )
+
     request = {
         "class": "d1",
         "dataset": "climate-dt",
@@ -91,18 +66,17 @@ def get_data(model, param, date):
         "expver": "0001",
         "stream": "clte",
         "time": "0000",
-        "date": date,
+        "date": dates,
         "type": "fc",
         "levtype": "o2d",
-        # "levelist": "1/2/3/4/5/6/7/8/9/10",
         "param": param,
         "feature": {
             "type": "polygon",
-            "shape": AOI["geometry"]["coordinates"],
+            "shape": shape,
         },
     }
 
-    return earthkit.data.from_source(
+    data = earthkit.data.from_source(
         "polytope",
         "destination-earth",
         request,
@@ -110,26 +84,30 @@ def get_data(model, param, date):
         address="polytope.lumi.apps.dte.destination-earth.eu",
     )
 
+    data = data.to_xarray()
 
-model = "IFS-NEMO"
-
-year = 2026
-month = 6
-dates = get_month_dates(year, month)
-
-param = "263101"
-data = get_data(model, param, dates)
+    data.to_zarr(f"data/{model}-{param}-{dates[:4]}-{dates[4:6]}.zarr")
+    data
 
 
-data = data.to_xarray()
-data.to_netcdf(f"data/{model}-{param}-{year}-{month}.nc")
+aoi = [[40.92, -14.65], [40.92, 9.40], [56.74, 9.40], [56.74, -14.65], [40.92, -14.65]]
 
-param = "263100"
-data = get_data(model, param, dates)
-data = data.to_xarray()
-data.to_netcdf(f"data/{model}-{param}-{year}-{month}.nc")
+combos = [
+    ("ICON", "263100", year, month, aoi)
+    for year in range(2025, 2040)
+    for month in range(1, 13)
+]
 
+combo = combos[0]
+data = get_data(combo[0], combo[1], get_month_dates(combo[2], combo[3]), combo[4])
 
-ds = xr.open_mfdataset("data/IFS-NEMO-263100-june.nc")
-
-print(ds)
+with ThreadPoolExecutor() as pool:
+    pool.map(
+        lambda combo: get_data(
+            combo[0],  # model
+            combo[1],  # param
+            get_month_dates(combo[2], combo[3]),  # dates string for year/month
+            combo[4],  # aoi shape
+        ),
+        combos,
+    )
